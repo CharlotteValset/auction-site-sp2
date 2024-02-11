502bab80e6e3988b311945528510c74093a3febb
Charlotte Valset <charlottevalset@gmail.com>
2024-02-11 18:00:00 +0100

    adds custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,mjs}", "./*.html", "./node_modules/flowbite/**/*.js"],
+  content: [
+    "./src/**/*.{html,js,mjs}",
+    "./*.html",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-grey": "#F5F5F5",
+        "green-primary": "#1D7671",
+        "green-primary-hover": "#009C93",
+        "darkest-grey": "#444444",
+        "lighter-grey": "#BBBBBB",
+        "almost-white": "#FEFEFE",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
