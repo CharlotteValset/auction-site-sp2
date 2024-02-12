5c58ab3e85fa39c2254da72748589fccac0bb29d
Charlotte Valset <charlottevalset@gmail.com>
2024-02-12 10:56:27 +0100

    Added paths to vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,16 @@
 // vite.config.js
+import path from "path";
+
 export default {
-  // config options
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        login: path.resolve(__dirname, "log-in/index.html"),
+        signup: path.resolve(__dirname, "sign-up/index.html"),
+        listing: path.resolve(__dirname, "listing/index.html"),
+        profile: path.resolve(__dirname, "profile/index.html"),
+      },
+    },
+  },
 };
