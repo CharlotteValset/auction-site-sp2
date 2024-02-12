66baff5f87e6db454d42da52204c54fb2820ac1b
Charlotte Valset <charlottevalset@gmail.com>
2024-02-12 11:51:18 +0100

    adds folder to content config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
 const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: [
     "./src/**/*.{html,js,mjs}",
-    "./*.html",
+    "./**/*.html",
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
