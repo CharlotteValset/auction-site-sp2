e7c867e056075b2b609cc8062e90141c769abcdd
Charlotte Valset <charlottevalset@gmail.com>
2024-02-21 10:59:12 +0100

    Adds fetcher for profiledata

---
+++ src/js/pages/profile/fetchUserProfile.mjs
@@ -0,0 +1,13 @@
+import { apiBaseUrl, profileUrl } from "../../variables.mjs";
+import { fetchWithToken } from "../../auth/accesstoken.mjs";
+
+// Retrieve user profile information from localStorage
+const user = JSON.parse(localStorage.getItem("userProfile"));
+
+/**
+ * Fetches the user profile data with an access token.
+ * @returns {Promise<UserProfile>} - A promise that resolves to the user's profile data.
+ */
+export const fetchUserProfile = async () => {
+  return await fetchWithToken(`${apiBaseUrl}${profileUrl}${user.name}?`);
+};
