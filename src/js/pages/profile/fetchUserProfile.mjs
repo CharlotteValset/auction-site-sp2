bda0a294a7747c3a92b0b693a4f4bd6a9d616150
Charlotte Valset <charlottevalset@gmail.com>
2024-02-22 09:01:19 +0100

    adds username to url

--- src/js/pages/profile/fetchUserProfile.mjs
+++ src/js/pages/profile/fetchUserProfile.mjs
@@ -2,12 +2,17 @@ import { apiBaseUrl, profileUrl } from "../../variables.mjs";
 import { fetchWithToken } from "../../auth/accesstoken.mjs";
 
 // Retrieve user profile information from localStorage
-const user = JSON.parse(localStorage.getItem("userProfile"));
+//const user = JSON.parse(localStorage.getItem("userProfile"));
+
+const userProfileString = localStorage.getItem("userProfile");
+const userProfileObject = JSON.parse(userProfileString);
+// Access the "name" property
+const userName = userProfileObject.name;
 
 /**
  * Fetches the user profile data with an access token.
  * @returns {Promise<UserProfile>} - A promise that resolves to the user's profile data.
  */
 export const fetchUserProfile = async () => {
-  return await fetchWithToken(`${apiBaseUrl}${profileUrl}/${user.name}?`);
+  return await fetchWithToken(`${apiBaseUrl}${profileUrl}/${userName}`);
 };
