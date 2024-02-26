81d0280a3376a40c33e6dfd38ca904e151006149
Charlotte Valset <charlottevalset@gmail.com>
2024-02-26 22:26:50 +0100

    adds toggle loggedIn or out to navbar

--- src/js/pages/homepage/index.mjs
+++ src/js/pages/homepage/index.mjs
@@ -1,10 +1,22 @@
-import { logout, logOutButton } from "../../auth/logout.mjs";
+import { token } from "../../auth/accesstoken.mjs";
+import { isLoggedIn, isLoggedOut } from "../../auth/isUserLoggedIn.mjs";
 import {
   createListing,
   createListingForm,
 } from "../../listings/addNewListing.mjs";
 import { displayAllListings } from "../../listings/displayListings.mjs";
 import { initialize } from "../../listings/searchListings.mjs";
+import { logout, logOutButton } from "../../auth/logout.mjs";
+
+const notLoggedInNavBar = document.querySelectorAll(".isNotLoggedIn");
+const isLoggedInNavBar = document.querySelectorAll(".isLoggedIn");
+
+isLoggedInNavBar.forEach((element) => {
+  isLoggedIn(token, element);
+});
+notLoggedInNavBar.forEach((element) => {
+  isLoggedOut(token, element);
+});
 
 // Initial call to display blog cards
 displayAllListings();
@@ -15,4 +27,6 @@ initialize();
 logOutButton.addEventListener("click", logout);
 
 // Add an event listener to the form for the submit event
-createListingForm.addEventListener("submit", createListing);
+if (token) {
+  createListingForm.addEventListener("submit", createListing);
+}
