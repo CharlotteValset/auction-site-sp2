c2cd976db35a5055218054e6b3abb917388028c3
Charlotte Valset <charlottevalset@gmail.com>
2024-02-29 10:34:12 +0100

    fixes and cleans up function to view user-wins

--- src/js/pages/profile/displayUserWins.mjs
+++ src/js/pages/profile/displayUserWins.mjs
@@ -1,41 +1,8 @@
-import { apiBaseUrl, profileUrl, allListingsUrl } from "../../variables.mjs";
+import { apiBaseUrl, profileUrl } from "../../variables.mjs";
 import { fetchWithToken, getData } from "../../auth/accesstoken.mjs";
+import { fetchSingleListing } from "../singleListing/singleListingPage.mjs";
 import { createMessage } from "../../errorHandling/errorMessage.mjs";
-//import placeholderImg from "../../../../images/no_img.jpg";
-
-/* const queryString = document.location.search;
-const params = new URLSearchParams(queryString);
-const id = params.get("id"); */
-
-const fetchSingleListing = async (id) => {
-  return await fetchWithToken(
-    `${apiBaseUrl}${allListingsUrl}/${id}?_bids=true`,
-    getData,
-  );
-};
-
-const createUserWinsCard = async (data) => {
-  // Create a card container
-  const cardContainer = document.createElement("div");
-  cardContainer.className =
-    "user-bid-card bg-white p-4 mb-4 rounded-md shadow-md";
-
-  const winTitle = document.createElement("a");
-  winTitle.className = "text-lg font-semibold mb-2";
-
-  if (data) {
-    const singleListing = await fetchSingleListing(data);
-    console.log(singleListing);
-  } else {
-    winTitle.textContent = "No listings won yet";
-  }
-
-  console.log("Data:", data);
-
-  cardContainer.appendChild(winTitle);
-
-  return cardContainer;
-};
+import { listingsCard } from "../../components/listingCard.mjs";
 
 // Retrieve user profile information from localStorage
 const user = JSON.parse(localStorage.getItem("userProfile"));
@@ -46,7 +13,6 @@ export const fetchUserWins = async () => {
     getData,
   );
 };
-console.log("user", user.name);
 
 // Targeting DOM elements
 const loaderContainer = document.querySelector("#user-win-loader-container");
@@ -82,9 +48,6 @@ export const displayUserWins = async () => {
     // Fetch listings
     const userWinsOnListings = await fetchUserWins();
 
-    // Sort the listings by endsAt date using your sortAcs function
-    console.log("userWinsListings", userWinsOnListings);
-
     // Clear existing cards from the container
     userWinsOnListingsContainer.innerHTML = "";
 
@@ -94,24 +57,23 @@ export const displayUserWins = async () => {
       Array.isArray(userWinsOnListings.wins) &&
       userWinsOnListings.wins.length > 0
     ) {
-      // Iterate over each user bid and create a card for each
-      userWinsOnListings.wins.forEach((data) => {
-        // Create a card element for the current user bid data
-        const bidCard = createUserWinsCard(data);
-        // Append the generated card to the container for user bids
-        userWinsOnListingsContainer.appendChild(bidCard);
+      // Iterate over each user win and create a card for each
+      userWinsOnListings.wins.forEach(async (data) => {
+        const getSingleListing = await fetchSingleListing(data);
+        const userWinCard = listingsCard(getSingleListing);
+        // Append the generated card to the container for user wins
+        userWinsOnListingsContainer.appendChild(userWinCard);
       });
     } else {
-      // Display a message for no bids
+      // Display a message for no wins
       const infotext = document.createElement("p");
       infotext.className =
         "text-center text-green-primary mx-auto my-2 lg:text-left";
-      infotext.innerHTML = "You haven't made any bids yet";
+      infotext.innerHTML = "You haven't won an auction yet";
       noUserWinsOnListingsContainer.appendChild(infotext);
     }
   } catch (error) {
     // Display error message
-    console.log("something here");
     loaderContainer.innerHTML = errorMessage;
     // Throw a new error
     throw new Error(error);
